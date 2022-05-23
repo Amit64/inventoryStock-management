0fdbdf8a89e4f32fce7ba843d63756527320f053
Amit DebNath <amitdebnath@Amits-MacBook-Pro.local>
2022-05-23 19:15:13 +0600

    add 404 page

--- src/components/shared/PageNotFound/PageNotFound.js
+++ src/components/shared/PageNotFound/PageNotFound.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Lottie from "react-lottie";
+import boterror404 from '../../../Assets/Lottie/boterror404.json';
 
 const PageNotFound = () => {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: boterror404,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice",
+        },
+      };
     return (
-        <div>
-            <h2>page not found</h2>
+        <div className=''>
+            <div className=' w-2/4 px-5 py-24 mx-auto '> 
+            <h1 className='text-5xl text-center'>Page Not Found</h1>             
+            <Lottie options={defaultOptions}></Lottie>
+            </div>
         </div>
     );
 };
